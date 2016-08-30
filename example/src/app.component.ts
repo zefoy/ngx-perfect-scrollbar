50b0d10250bf8f5fc4419c97e257a065961381d8
Janne Julkunen <janne.julkunen@zef.fi>
2016-08-30 12:18:59 +0300

    Made the example app prettier

--- example/src/app.component.ts
+++ example/src/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  private title = 'Simple test app';
+  private title = 'Simple example app for the angular2-perfect-scrollbar';
 
   constructor() {}
 }
