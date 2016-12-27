05792589447da05370629cc201f87bc33a880b3b
Janne Julkunen <janne.julkunen@zef.fi>
2016-12-27 12:04:26 +0200

    Run all perfect scrollbar functions outside angular zone

--- src/lib/perfect-scrollbar.component.ts
+++ src/lib/perfect-scrollbar.component.ts
@@ -41,13 +41,19 @@ export class PerfectScrollbarComponent implements DoCheck, OnDestroy, AfterViewI
         this.contentWidth = contentWidth;
         this.contentHeight = contentHeight;
 
-        Ps.update(this.elementRef.nativeElement);
+        this.update();
       }
     }
   }
 
   ngOnDestroy() {
-    Ps.destroy(this.elementRef.nativeElement);
+    if (this.runInsideAngular) {
+      Ps.destroy(this.elementRef.nativeElement);
+    } else {
+      this.zone.runOutsideAngular(() => {
+        Ps.destroy(this.elementRef.nativeElement);
+      });
+    }
   }
 
   ngAfterViewInit() {
@@ -58,19 +64,25 @@ export class PerfectScrollbarComponent implements DoCheck, OnDestroy, AfterViewI
     if (this.runInsideAngular) {
       Ps.initialize(this.elementRef.nativeElement, config);
     } else {
-       this.zone.runOutsideAngular(() => {
+      this.zone.runOutsideAngular(() => {
         Ps.initialize(this.elementRef.nativeElement, config);
       });
     }
   }
 
   update() {
-    Ps.update(this.elementRef.nativeElement);
+    if (this.runInsideAngular) {
+      Ps.update(this.elementRef.nativeElement);
+    } else {
+      this.zone.runOutsideAngular(() => {
+        Ps.update(this.elementRef.nativeElement);
+      });
+    }
   }
 
   scrollTo(position: number) {
     this.elementRef.nativeElement.scrollTop = position;
 
-    Ps.update(this.elementRef.nativeElement);
+    this.update();
   }
 }
