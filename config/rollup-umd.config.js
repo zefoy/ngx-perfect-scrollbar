a4560b679cfc00a0439f4895f09c187884872ab5
Janne Julkunen <janne.julkunen@zef.fi>
2017-12-17 20:26:40 +0200

    Don't include perfect-scrollbar in the umd bundle

--- config/rollup-umd.config.js
+++ config/rollup-umd.config.js
@@ -157,7 +157,9 @@ const globals = {
   'rxjs/operator/windowWhen': 'Rx.Observable.prototype',
   'rxjs/operator/withLatestFrom': 'Rx.Observable.prototype',
   'rxjs/operator/zip': 'Rx.Observable.prototype',
-  'rxjs/operator/zipAll': 'Rx.Observable.prototype'
+  'rxjs/operator/zipAll': 'Rx.Observable.prototype',
+
+  'perfect-scrollbar': 'PerfectScrollbar'
 };
 
 const external = Object.keys(globals);
